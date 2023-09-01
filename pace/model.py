7d4aaee1b18fb40785faef6550ce93dd69babb95
awcheng23 <awcheng23@gmail.com>
2023-09-01 18:15:46 -0400

    Initialize training
    - Created initial model code
    - Setup training file

---
+++ pace/model.py
@@ -0,0 +1,15 @@
+import torch as th
+import torch.nn as nn
+
+"""
+Author: Alice Cheng, Josue N Rivera
+Date: 9/1/2023
+"""
+
+class Pace(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(x):
+        pass
+    
---
+++ train.py
@@ -0,0 +1,10 @@
+import torch as th
+import torch.optim as optim
+import pace.db as db
+
+"""
+Author: Alice Cheng, Josue N Rivera
+Date: 9/1/2023
+"""
+
+dt = db.Database("./data/db.npz")
