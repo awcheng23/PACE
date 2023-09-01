a10b38c66121d1af5716c8f69442fec7e48a7a41
Josue N Rivera <josue.n.rivera@outlook.com>
2023-09-01 17:25:05 -0400

    Complete file load to pytorch database

--- pace/db.py
+++ pace/db.py
@@ -11,29 +11,23 @@ Date: 8/22/2023
 class ArrhythmiaDatabase(Dataset):
 
     def __init__(self, 
-                 path:str = "../data/db.npy") -> None:
+                 path:str = "../data/db.npz") -> None:
         super().__init__()
-        
-        imgs = []
-        ncount = []
 
-        ## TODO: load data from numpy file
-        
-        self.nclass = len(ncount)
-        self.n = sum(ncount)
+        npzfile = np.load(path)
+        segments = npzfile.segments
+        labels = npzfile.labels
+        self.n = len(segments)
 
-        self.imgs = th.tensor(np.array(imgs, dtype = np.float32))
-        self.labels = th.concat([i*th.ones(ncount[i], dtype=th.long) for i in range(len(ncount))], dim=0)
-
-    def get_nclass(self) -> int:
-        return self.nclass
+        self.segments = th.tensor(segments)
+        self.labels = th.tensor(labels)
 
     def __len__(self) -> int:
-        return self.n
+        return self.n 
 
     def __getitem__(self, 
                     idx: int) -> Tuple[th.Tensor, th.Tensor]:
-        return self.imgs[idx], self.labels[idx]
+        return self.segments[idx], self.labels[idx]
 
 if __name__ == "__main__":
     dt = ArrhythmiaDatabase()
