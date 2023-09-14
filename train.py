b2edbf5f4145195ffab1f154017aaac143ceffa7
awcheng23 <awcheng23@gmail.com>
2023-09-14 08:51:55 -0400

    Add framework for model training - Loss function - Optimizer - Data loader

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch as th
 import torch.optim as optim
 import pace.db as db
+import pace.model as md
 
 """
 Author: Alice Cheng, Josue N Rivera
@@ -8,6 +9,15 @@ Date: 9/1/2023
 """
 
 dt = db.Database("./data/db.npz")
+
+nn = md.Pace()
+
+batch_size = 4
+
+trainloader = th.utils.data.DataLoader(dt, batch_size=batch_size, shuffle=True, num_workers=2)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) # ??
 
 for epoch in range(2):  # loop over the dataset multiple times
 
