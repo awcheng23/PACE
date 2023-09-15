c85cbaae4bf8ec3d268267adeb27fa62fd8bfd0c
Josue <josue.n.rivera@outlook.com>
2023-09-15 01:40:31 -0400

    Restructure preprocessing (#4)
    * Reformat preprocessing as part of the pace library
    
    * Remove unnecessary libraries calls
    
    * Standardize database file
    
    * Remove redundant code
    
    * Format fix

---
+++ preprocessing.py
@@ -0,0 +1,35 @@
+import numpy as np
+from pace.db import get_patients_beats, cwt_parallel, padd_scalograms
+from pace import PATIENT_IDS
+
+"""
+Author: Alice Cheng, Josue N Rivera
+Date: 8/22/2023
+"""
+
+def main():
+    beats = []
+    beat_IDs = []
+    widths = np.arange(1, 31)
+    data_path = 'data/mitdb/'
+
+    for id in PATIENT_IDS[0:1]:
+        pat_beats, pat_beat_ID = get_patients_beats(ID=id, dt_path=data_path)
+
+        beats.extend(pat_beats)
+        beat_IDs.extend(pat_beat_ID)
+
+    print("Completed data load")
+
+    scalograms = cwt_parallel(beats=beats, widths=widths)
+    print("Completed cwt")
+
+    scalograms = padd_scalograms(scalograms)
+    print("Completed padding")
+
+    scalograms = np.array(scalograms)
+    np.savez("data/db.npz", scalograms=scalograms, labels=beat_IDs)
+    print("Completed file save")
+
+if __name__ == '__main__':
+    main()
