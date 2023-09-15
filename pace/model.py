65a859b2aa5a26b647d7be281db67383ef95bef8
Josue N Rivera <josue.n.rivera@outlook.com>
2023-09-15 01:13:46 -0400

    Restructure image sizing

--- pace/model.py
+++ pace/model.py
@@ -11,14 +11,13 @@ class Pace(nn.Module):
     def __init__(self, 
                  beat_length:int = 966,
                  widths:int = 31) -> None:
+        size = th.tensor([widths, beat_length])
+        size = ((size-4)//2-9)//2
+
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 10)
-
-        height = ((widths-4)//2-9)//2
-        width = ((beat_length-4)//2-9)//2
-
-        self.fc1 = nn.Linear(16 * height * width, 1000)
+        self.fc1 = nn.Linear(16 * size.prod(), 1000)
         self.fc2 = nn.Linear(1000, 500)
         self.fc3 = nn.Linear(500, 25)
         self.fc4 = nn.Linear(25, 5)
