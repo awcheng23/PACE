5e5376a6d27ad9740f3e3e22b3af32b819c53ca1
awcheng23 <awcheng23@gmail.com>
2023-09-26 20:50:53 -0400

    Fix text capitalization

--- pace/__init__.py
+++ pace/__init__.py
@@ -1,6 +1,6 @@
 
 ID_TO_BEAT = {0:['N','L','R','e','j'], 1:['S','A','a','J'], 2:['V','E'], 3:['F'], 4:['/','Q','f']}
-ID_TO_AAMI = {0:'Normal', 1:'Supraventricular premature', 2:'Premature ventricular contraction', 3:'Fusion of ventricular & normal', 4:'Unclassifiable'}
+ID_TO_AAMI = {0:'Normal', 1:'Supraventricular Premature', 2:'Premature Ventricular Contraction', 3:'Fusion of Ventricular & Normal', 4:'Unclassifiable'}
 
 PATIENT_IDS = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 118, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 207, 208, 209, 210, 212, 213, 214, 215, 217, 219, 220, 221, 222, 223, 228, 230, 231, 232, 233, 234]
 
